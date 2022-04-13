f79470e6822b805e4a685096b08418623265c983
gimjh003 <46261152+gimjh003@users.noreply.github.com>
2022-04-14 00:56:06 +0900

    python_web_practice

--- webscraping_basic/6_bs4.py
+++ webscraping_basic/6_bs4.py
@@ -18,5 +18,24 @@ soup = BeautifulSoup(res.text, "lxml")
 # print(soup.find(attrs={"class":"Nbtn_upload"})) # class="Nbtn_upload" 인 어떤 element를 찾아달라는 뜻이다. (태그를 지정하지 않아도 가능)
 
 # print(soup.find("li", attrs={"class":"rank01"}))
-rank1 = soup.find("li", attrs={"class":"rank01"})
-print(rank1.a) # li element 내부에 있는 a element를 가져옴
+# rank1 = soup.find("li", attrs={"class":"rank01"})
+# print(rank1.a) # li element 내부에 있는 a element를 가져옴
+# print(rank1.a.get_text())
+# print(rank1.next_sibling)
+# rank2 = rank1.next_sibling.next_sibling
+# rank3 = rank2.next_sibling.next_sibling
+# print(rank3.a.get_text())
+# rank2 = rank3.previous_sibling.previous_sibling
+# print(rank2.a.get_text())
+# print(rank1.parent)
+# rank2 = rank1.find_next_sibling("li") # 형제 태그를 찾을 때에도 조건 설정 가능
+# print(rank2.a.get_text())
+# rank3 = rank2.find_next_sibling("li")
+# print(rank3.a.get_text())
+# rank2 = rank3.find_previous_sibling("li")
+# print(rank2.a.get_text())
+
+# print(rank1.find_next_siblings("li"))
+
+webtoon = soup.find("a", text="")
+print(webtoon)
---
+++ webscraping_basic/8_bs4_sound.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://comic.naver.com/webtoon/list?titleId=20853"
+res = requests.get(url)
+res.raise_for_status()
+
+soup = BeautifulSoup(res.text, "lxml")
+cartoons = soup.find_all("td", attrs={"class":"title"})
+
+for cartoon in cartoons:
+    title = cartoon.a.get_text()
+    link = "https://comic.naver.com"+cartoon.a["href"]
+    print(title)
+    print(link)
+    print()
