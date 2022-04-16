28f95b9c6e624777dac49bc0c68dfc0d44ccc8d0
gimjh003 <46261152+gimjh003@users.noreply.github.com>
2022-04-16 17:33:36 +0900

    Update 18_headless_chrome_useragent.py

--- webscraping_basic/18_headless_chrome_useragent.py
+++ webscraping_basic/18_headless_chrome_useragent.py
@@ -1,8 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import time
-
 options = webdriver.ChromeOptions()
 options.headless = True
 options.add_argument("window-size=1920x1080")
